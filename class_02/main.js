b9dd15033c4cbf82269e9b4d6afb0b286c202436
Sayeed Afridi <as.afridi563@gmail.com>
2020-02-26 16:43:00 +0600

    second class

---
+++ class_02/main.js
@@ -0,0 +1,67 @@
+//variable mutation
+
+var age = 23
+age = 'twenty three'
+var number = 25
+console.log(age + ' is not equal to ' + number)
+
+//operators
+
+//math operators -,+, *, /
+//BMI calculator
+
+//logical operators
+//&& -> and
+//|| -> or
+//!= -> not equal
+// var weight = prompt('Your Weight') //kg
+// var height = prompt('Your height') //meter
+
+// var BMI = weight / (height * height)
+// alert('Your BMI = ' + BMI)
+
+// if (BMI <= 18.5) {
+//     alert("You're underweight")
+// } else if (BMI > 18.5 && BMI <= 24.9) {
+//     alert("You're Healthy")
+// } else if (BMI > 24.9 && BMI <= 29.9) {
+//     alert("You're Overweught")
+// } else {
+//     alert("You're fat")
+// }
+
+//loops
+
+//for loop
+// for (var i = 50; i >= 1; i--) {
+//     console.log(i)
+// }
+
+//var i = 50
+// while loop
+// while (i >= 1) {
+//     if (i === 20) {
+//         i--
+//         continue
+//     }
+//     console.log(i)
+//     i--
+// }
+// do... while loop
+
+// do {
+//     console.log(i)
+//     i--
+// } while (i >= 1)
+
+//function
+
+// function bmiCalculator(w, h) {
+//     var bmi = w / (h * h)
+//     return bmi
+// }
+
+// var calculatedBMI = bmiCalculator(weight, height)
+
+//alert('Your BMI = ' + calculatedBMI)
+//dom -> Document object model
